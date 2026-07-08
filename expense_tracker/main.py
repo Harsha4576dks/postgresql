42b97d080e25b39cf6b416f58f933189bf5005a9
Harsha4576dks <harshivahm@gmail.com>
2026-07-08 20:32:53 +0530

    expense_tracker

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -4,13 +4,28 @@ from typing import List, Annotated
 from . import models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from datetime import date
 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+class UserBase(BaseModel):
+    name:str
+    salary:int
+    
 class ExpenseBase(BaseModel):
-    date:str
-    amount:int
-    category:str
+    date:date
+    description:str
     payment_method:str
+    amount_spent:int
+    user_id:int
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency  = Annotated[Session, Depends(get_db)]
--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -1,3 +1,24 @@
-from sqlalchemy import Boolean,Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean,Column, ForeignKey, Integer, String, Date
 from .database import Base
+from sqlalchemy.orm import relationship
 
+class Expense(Base):
+    __tablename__='expenses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, index=True)
+    description = Column(String, index=True)
+    payment_method = Column(String, index=True)
+    amount_spent = Column(Integer, index=True)
+    User_id = Column(Integer, ForeignKey("user_data.id"))
+    user = relationship("User", back_populates="expenses")
+
+class User(Base):
+    __tablename__='user_data'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    salary = Column(Integer, index=True)
+    expenses = relationship("Expense", back_populates="user")
+
+    
