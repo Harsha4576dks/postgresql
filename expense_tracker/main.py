1cbca4b3333d2440451b7563f1c68b17d93d8787
Harsha4576dks <harshivahm@gmail.com>
2026-07-09 15:09:39 +0530

    expense_tracker

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -50,3 +50,61 @@ async def create_expense(expense:ExpenseBase, db:db_dependency):
     db.commit()
     db.refresh(db_expense)
     return db_expense
+
+@app.get("/users/{user_id}")
+async def read_user(user_id:int, db:db_dependency):
+    result = db.query(models.User).filter(models.User.id == user_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail='user  not found')
+    return result
+
+@app.get("/expenses/{expense_id}")
+async def read_expense(expense_id:int, db:db_dependency):
+    result = db.query(models.Expense).filter(models.Expense.id == expense_id).all()
+    if not result:
+        raise HTTPException(status_code=404, detail='expenses not found')
+    return result
+
+@app.put("/expenses/{expense_id}")
+async def update_expense(expense_id:int, expense:ExpenseBase, db:db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == expense.user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='expense not found')
+    
+    
+    db_expense = db.query(models.Expense).filter(models.Expense.id == expense_id).first()
+    if db_expense is None:
+        raise HTTPException(status_code=404, detail='expense not found')
+    
+    db_expense.date=expense.date, 
+    db_expense.description=expense.description,
+    db_expense.payment_method=expense.payment_method,
+    db_expense.amount_spent=expense.amount_spent,
+    db_expense.user_id=expense.user_id
+
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+@app.delete("/expenses/{expense_id}")
+async def delete_expense(expense_id:int, db:db_dependency):
+    db_expense = db.query(models.Expense).filter(models.Expense.id == expense_id).first()
+    if db_expense is None:
+        raise HTTPException(status_code=404, detail='expense does not exist')
+    db.delete(db_expense)
+    db.commit()
+    return {"message":"expenses deleted successfully", "deleted_expense_id":db_expense.id}
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id:int, db:db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='user not found')
+    
+    db_expense = db.query(models.Expense).filter(models.Expense.user_id == user_id).first()
+    if db_expense is not None:
+        raise HTTPException(status_code=404, detail='This user has expense delete the expenses first')
+    
+    db.delete(db_user)
+    db.commit()
+    return {"message":"user deleted successfully", "deleted_user_id":db_user.id}
